fee413001b6f132eca945b7d3661e8e5ba22e10d
Mathal Fonteyne <mathalathieu@gmail.com>
2023-12-28 23:57:07 -0500

    Sum arrays challenge and solution.

---
+++ 8kyu/sumArrays.js
@@ -0,0 +1,46 @@
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0.
+// What We're Testing
+// We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+// Advanced users may find this extremely easy and can easily write this in one line.
+
+
+/// My Solution ///
+
+function sum(numbers) {
+    "use strict";
+
+    // Check if the array is empty
+    if (numbers.length === 0) {
+        return 0;
+    }
+
+    // Use the reduce function to calculate the sum of the array elements
+    const totalSum = numbers.reduce((accumulator, currentValue) => {
+        // Check if the current value is a valid number
+        if (typeof currentValue === 'number' && !isNaN(currentValue)) {
+            return accumulator + currentValue;
+        } else {
+            // Ignore non-numeric values
+            return accumulator;
+        }
+    }, 0);
+
+    return totalSum;
+}
+
+// console.log(sum([1, 5.2, 4, 0, -1, 19])); // 28.2
